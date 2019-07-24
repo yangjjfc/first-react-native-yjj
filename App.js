880b3fe0ff833612b5d51f7c1efbe12322b2bd70
PV-X00211756\yangjj <1542103461@qq.com>
2019-07-24 23:54:15 +0800

    update

--- App.js
+++ App.js
@@ -24,6 +24,8 @@ import {
   ReloadInstructions
 } from "react-native/Libraries/NewAppScreen";
 
+import Home from './src/pages/home/index';
+
 const App = () => {
   return (
     <Fragment>
@@ -32,16 +34,17 @@ const App = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}
-        >
+          >
           <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes1231231231231</Text>
+              <Text style={styles.footer}>Engine: Hermes1asdadsda</Text>
             </View>
           )}
           <View style={styles.body}>
+          <Home/>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One12341243</Text>
+              <Text style={styles.sectionTitle}>Step 54654eqweqweqw646878</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
